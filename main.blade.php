bc582d61393011328dd52deca473e546ac786873
OmarYassin22 <97139249+OmarYassin22@users.noreply.github.com>
2022-05-18 15:39:44 +0200

    Add files via upload

---
+++ main.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>appointment</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+
+<body>
+
+
+    <header class="header fixed-top">
+
+        <div class="container">
+
+            <div class="row align-items-center justify-content-between">
+          
+                    <a href="#home" class="logo">Health<span>Care.</span></a>
+
+                    <nav class="nav">
+                        <a href="#home">home</a>
+                        <a href="#about">about</a>
+                        <a href="#services">services</a>
+                        <a href="#reviews">reviews</a>
+                        <a href="#contact">contact</a>
+                    </nav>
+
+                    <a href="#contact" class="link-btn">make appointment</a>
+                    <div id="menu-btn" class="fas fa-item"></div>
+            </div>
+
+        </div>
+
+
+
+        </div>
+        </div>
+
+
+
+        <script src="/resources/js/app.js"></script>
+</body>
+
+</html>
